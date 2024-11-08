9e879fbe0ff6481345726227d67a427c3ab3d4d2
honginSon <92095858+honginSon@users.noreply.github.com>
2024-11-08 18:35:57 +0900

    Refactor: 회원가입 Validation 중복 코드 제거

--- src/main/java/com/elice/artBoard/member/entity/MemberPostDto.java
+++ src/main/java/com/elice/artBoard/member/entity/MemberPostDto.java
@@ -13,8 +13,8 @@ public class MemberPostDto {
     private String name;
 
     @NotBlank(message = "이메일을 입력해 주세요")
-    // Todo: 이메일 형식 처리 안됨
-//    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "올바른 이메일 형식이 아닙니다.")
+    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$",
+            message = "올바른 이메일 형식이 아닙니다.")
     private String email;
 
     @NotBlank(message = "닉네임을 입력해 주세요")
