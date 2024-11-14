1fb716d95987919f83fb6900ae969bbd16feadab
honginSon <92095858+honginSon@users.noreply.github.com>
2024-11-14 11:06:09 +0900

    Feat: 회원 탈퇴 시 작성한 게시판 삭제 추가

--- src/main/java/com/elice/artBoard/board/repository/BoardRepository.java
+++ src/main/java/com/elice/artBoard/board/repository/BoardRepository.java
@@ -1,13 +1,17 @@
 package com.elice.artBoard.board.repository;
 
 import com.elice.artBoard.board.domain.Board;
+import com.elice.artBoard.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Query("select b from Board b join fetch b.member where b.id = :boardId")
     Optional<Board> findById(Long boardId);
+
+    List<Board> findByMember(Member member);
 }
